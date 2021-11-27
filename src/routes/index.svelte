077eb4836127cb83c6583d9515c2b605244f6716
ghanithan <ghanithan.subramani@gmail.com>
2021-11-27 19:57:43 +0530

    resolving path in multipage static app

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,14 @@
+
+<script>
+
+
+    import { page } from '$app/stores';
+    let  path_genart = $page.path + "/genart";
+
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<a href="/genart" > click here </a>
+<a href= {path_genart}  > click here </a>
