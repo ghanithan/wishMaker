4bdb3cee918c74f0c19c2a691e3ed1624895ea40
ghanithan <ghanithan.subramani@gmail.com>
2021-11-30 09:27:37 +0530

    getting it work with git pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 //import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
-
+const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -11,7 +11,12 @@ const config = {
 		}),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		paths: { 
+			base: dev ? '' :"/wishMaker" 
+		},
+	
+		
 	}
 };
 
